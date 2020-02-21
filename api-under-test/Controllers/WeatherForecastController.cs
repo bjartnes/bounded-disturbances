a0cc22b8ce1b74387e387f3d599d178ad7569b3a
Bjørn Einar Bjartnes <Bjorn.Einar.Bjartnes@nrk.no>
2020-02-21 10:04:47 +0100

    Challenge 1

--- api-under-test/Controllers/WeatherForecastController.cs
+++ api-under-test/Controllers/WeatherForecastController.cs
@@ -29,17 +29,22 @@ namespace api_under_test.Controllers
         [HttpGet]
         public async Task<IEnumerable<WeatherForecast>> Get()
         {
-            var retryPolicy = Policy.Handle<Exception>().RetryAsync(4);
-            var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromMilliseconds(300));
-            var chaosPolicy = MonkeyPolicy.InjectLatencyAsync(with =>
+           var chaosPolicy = MonkeyPolicy.InjectLatencyAsync(with =>
                 with.Latency(TimeSpan.FromSeconds(1))
                     .InjectionRate(0.1)
                     .Enabled(true));
-            var mix = Policy.WrapAsync(retryPolicy, timeoutPolicy, chaosPolicy);
+            var mix = Policy.WrapAsync(GetPolicy(), chaosPolicy);
             return await mix.ExecuteAsync(GetForecasts);
         }
 
-
+        private IAsyncPolicy GetPolicy() {
+            // FIll inn answer here
+//            var retryPolicy = Policy.Handle<Exception>().RetryAsync(4);
+//            var timeoutPolicy = Policy.TimeoutAsync(TimeSpan.FromMilliseconds(100));
+//            var policy = Policy.WrapAsync(retryPolicy, timeoutPolicy);
+            var policy = Policy.Handle<Exception>().RetryAsync(0);
+            return policy; 
+        }
         // Should probably get them from yr , but will of course cause a lot of load
         // from a workshop... Perhaps better to simulate
         private async Task<IEnumerable<WeatherForecast>> GetForecasts()
--- load-tests/Program.fs
+++ load-tests/Program.fs
@@ -1,4 +1,5 @@
 open Tests
+// To run demos uncomment the tests you want to run, start with challegne 1
 
 [<EntryPoint>]
 let main args =

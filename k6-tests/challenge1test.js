33d348fe57480075e04747c68f631a7b1cab3917
Bjørn Einar Bjartnes <Bjorn.Einar.Bjartnes@nrk.no>
2020-05-20 11:31:50 +0200

    Test 1 works too

---
+++ k6-tests/challenge1test.js
@@ -0,0 +1,26 @@
+import http from "k6/http";
+import { Rate } from "k6/metrics";
+import { Counter } from "k6/metrics";
+
+export let options = {
+  vus       : 50,
+  duration  : "6s",
+//  rps       : 25, //max requests per second, increase to go faster
+  insecureSkipTLSVerify : true, //ignore that localhost cert doesn't match host.docker.internal
+  thresholds: {
+    '200 OK rate': ['rate>0.99'],
+    '200 OK count': ['count>200'],
+    'http_req_duration': ['p(95)<200']
+ }
+}
+
+const myOkRate = new Rate("200 OK rate");
+const myOkCounter = new Counter("200 OK count");
+
+export default function() {
+  let response = http.get("https://host.docker.internal:5001/weatherforecast_challenge1");
+  let resOk = response.status === 200;
+  myOkRate.add(resOk);
+  myOkCounter.add(resOk);
+};
+

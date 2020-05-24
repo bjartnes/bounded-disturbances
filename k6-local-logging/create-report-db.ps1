5456759d881698f4250a914acce46b24780b77e2
Bjørn Einar Bjartnes <Bjorn.Einar.Bjartnes@nrk.no>
2020-05-24 11:09:26 +0200

    try to not set up volumes

--- k6-local-logging/create-report-db.ps1
+++
@@ -1,3 +0,0 @@
-
-curl -X POST -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE reportdb"
-
--- k6-local-logging/setup-volumes.ps1
+++
@@ -1,9 +0,0 @@
-echo "Setting up volumes"
-
-docker network create monitoring
-docker volume create grafana-volume
-docker volume create influxdb-volume
-docker volume create telegraf-volume #--opt type=none --opt device=/telegraf-volume --opt o=bind
-
-echo "Start Incluxdb and Grafana with 'docker-compose up'"
-echo "Refer to 'docker-compose.yml' info about ports. Login credentials for Grafana is 'admin'/'admin''"
